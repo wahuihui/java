b262fd769c3143a53b994b0930cbc09468445179
hui <1154437939@qq.com>
2022-02-06 16:18:56 +0800

    我的第一个Java程序

---
+++ java-foundational/src/main/java/com/hui/java/foundational/object/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.hui.java.foundational.object;
+
+/**
+ * 我的第一个Java程序
+ *
+ * @author hui 1154437939@qq.com
+ * @version 2022/2/6 16:14
+ * @since JDK8
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World with Windows10 & JDK8 & Git & Maven & IntelliJ IDEA 2021.2.3");
+
+    }
+}
