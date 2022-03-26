8316c2484db7ec4643e7b49dbd49fecb57072d3b
hui <1154437939@qq.com>
2022-03-26 17:55:18 +0800

    代理模式的协议 抢手机的接口

---
+++ java-foundational/src/main/java/com/hui/java/api/lang/reflect/proxy/Shopping.java
@@ -0,0 +1,19 @@
+package com.hui.java.api.lang.reflect.proxy;
+
+/**
+ * 代理模式的协议
+ * 抢手机的接口
+ *
+ * @author hui 1154437939@qq.com
+ * @version 2022/3/26 15:02
+ * @since JDK8
+ */
+
+public interface Shopping {
+
+    /**
+     * 买手机
+     */
+    void buyCellphone();
+
+}
