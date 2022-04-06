df7fdec1e5eb25bc9e90834c770b11e2b8241066
hui <1154437939@qq.com>
2022-04-06 17:15:14 +0800

    User 实体类

---
+++ java-web-foundational/src/main/java/com/hui/web/foundational/bean/dto/UserInfo.java
@@ -0,0 +1,15 @@
+package com.hui.web.foundational.bean.dto;
+
+/**
+ * User 实体类
+ *
+ * @author hui 1154437939@qq.com
+ * @version 2022/3/31 16:02
+ * @since JDK8
+ */
+
+public class UserInfo {
+
+
+
+}
